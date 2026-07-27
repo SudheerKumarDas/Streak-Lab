b4f82b80a3458d05844df32ba9f0e9af024eb816
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-27 22:14:26 +0545

    database connected

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:3000/${PORT}`);
-})
+  });
+});
---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,8 @@
+export const registerUser = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.error(`Error registering user`);
+        res.status
+    }
+}
